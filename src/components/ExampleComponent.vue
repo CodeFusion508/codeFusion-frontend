78503f66aa1a56ae15d0e3b2c6031c593fdcfe6e
Alfredo <61888719+Alfredo3232@users.noreply.github.com>
2023-02-18 12:33:19 -0600

    style: install bootstrap as CDN

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,3 +1,50 @@
 <template>
-  <p>something here</p>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a
+        class="navbar-brand"
+        href="#"
+      >Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div
+        id="navbarNav"
+        class="collapse navbar-collapse"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a
+              class="nav-link active"
+              aria-current="page"
+              href="#"
+            >Home</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              href="#"
+            >Features</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              href="#"
+            >Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
