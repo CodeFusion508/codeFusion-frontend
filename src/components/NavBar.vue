41d5cad041b9f37f337fe9a4cff876c03149deaa
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-10 15:15:30 -0600

    chore: update eslintrc and lint changes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a
+      class="navbar-brand"
+      href="#"
+    >CodeFusion 508</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div
+      id="navbarNavAltMarkup"
+      class="collapse navbar-collapse"
+    >
+      <div class="navbar-nav">
+        <a
+          class="nav-item nav-link active"
+          href="#"
+        >Home</a>
+        <a
+          class="nav-item nav-link"
+          href="#"
+        >About Us</a>
+        <a
+          class="nav-item nav-link"
+          href="#"
+        >Lessons</a>
+      </div>
+    </div>
+  </nav>
+</template>
