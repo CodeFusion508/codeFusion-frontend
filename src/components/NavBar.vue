4b5cb6fe797db9a7843a93bb4ca76078a01ee306
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-14 15:27:25 -0600

    chore: delete unnecessary vscode setting feat: change language to Spanish

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,28 +16,28 @@
       <div id="header1navbarCollapse" class="collapse navbar-collapse justify-content-between">
         <ul class="nav nav-tabs flex-grow-1">
           <li class="nav-item">
-            <a href="#" class="nav-link active">Home</a>
+            <a href="#" class="nav-link active">Inició</a>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">About Us</a>
+            <a href="#" class="nav-link">De Nosotros</a>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">Lessons</a>
+            <a href="#" class="nav-link">Lecciones</a>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">Articles</a>
+            <a href="#" class="nav-link">Artículos</a>
           </li>
         </ul>
         <div class="navbar-nav">
           <div class="bs_header_btn_wrapper bs_signin_btn_blk">
-            <a href="#" class="nav-item nav-link bs_signin_btn rounded">Sign In</a>
+            <a href="#" class="nav-item nav-link bs_signin_btn rounded">Iniciar Sesión</a>
           </div>
 
           <div class="bs_header_btn_wrapper bs_signup_btn_blk">
-            <a href="#" class="nav-item nav-link bg-primary text-white bs_signup_btn rounded">Sign Up</a>
+            <a href="#" class="nav-item nav-link bg-primary text-white bs_signup_btn rounded">Regístrate</a>
           </div>
         </div>
       </div>
--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -3,11 +3,7 @@
     <div class="container">
       <div class="d-flex bd-highlight mb-3">
         <div class="bd-highlight">
-          <img
-            src="../assets/CodeFusion508.ico"
-            alt="logo"
-            class="bs_footer_logo"
-          >
+          <img src="../assets/CodeFusion508.ico" alt="logo" class="bs_footer_logo">
         </div>
       </div>
 
@@ -15,64 +11,36 @@
       <div class="d-flex row justify-content-center py-4 bs_footer_row">
         <div class="col-xs-6  col-sm bs_footer_col">
           <h4 class="bs_footer_widget_title">
-            Top Features
+            Otros Productos
           </h4>
           <ul class="bs_footer_widget_links">
-            <li>
-              <a href="#" target="_blank" class="text-decoration-none text-reset"><span>Social media tools</span></a>
-            </li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Ads</span></a></li>
-            <li>
-              <a href="#" target="_blank" class="text-decoration-none text-reset"><span>Email automations</span></a>
-            </li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Themes</span></a></li>
+            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Backend API</span></a></li>
           </ul>
         </div>
 
         <div class="col-xs-6  col-sm bs_footer_col">
           <h4 class="bs_footer_widget_title">
-            Softwares
+            Necesitas Ayuda?
           </h4>
           <ul class="bs_footer_widget_links">
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Blogs</span></a></li>
-            <li>
-              <a href="#" target="_blank" class="text-decoration-none text-reset"><span>Landing page builder</span></a>
-            </li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Website Builder</span></a></li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Chat builder</span></a></li>
+            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Articulo 1</span></a></li>
+            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Articulo 2</span></a></li>
           </ul>
         </div>
 
         <div class="col-xs-6  col-sm bs_footer_col">
           <h4 class="bs_footer_widget_title">
-            Guides
+            Elances
           </h4>
           <ul class="bs_footer_widget_links">
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Starter template</span></a></li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Invoice template</span></a></li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Pipeline tools</span></a></li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Webpack</span></a></li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Icons</span></a></li>
+            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Documentación</span></a></li>
+            <li>
+              <a href="#" target="_blank" class="text-decoration-none text-reset"><span>GitHub</span></a>
+            </li>
           </ul>
         </div>
 
-        <div class="col-xs-6  col-sm bs_footer_col">
-          <h4 class="bs_footer_widget_title">
-            Links
-          </h4>
-          <ul class="bs_footer_widget_links">
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>Docs</span></a></li>
-            <li>
-              <a href="#" target="_blank" class="text-decoration-none text-reset"><span>Invesator relations</span></a>
-            </li>
-            <li>
-              <a href="#" target="_blank" class="text-decoration-none text-reset"><span>Board of directors</span></a>
-            </li>
-            <li><a href="#" target="_blank" class="text-decoration-none text-reset"><span>App programs</span></a></li>
-          </ul>
-        </div>
-
-        <div class="col-xs-6  col-sm bs_footer_col">
+        <!-- <div class="col-xs-6  col-sm bs_footer_col">
           <h4 class="bs_footer_widget_title">
             Community
           </h4>
@@ -84,7 +52,7 @@
               <a href="#" target="_blank" class="text-decoration-none text-reset"><span>Corporate sponsors</span></a>
             </li>
           </ul>
-        </div>
+        </div> -->
       </div>
 
       <hr>
@@ -92,7 +60,7 @@
         <div class="d-flex bd-highlight mb-3">
           <div class="bd-highlight bs_footer_copyright_1">
             <p class="bs_text_copyright bs_footer_layout_7_bs_text_copyright1">
-              Internet of things
+              Aprende A Programar
             </p>
           </div>
         </div>
