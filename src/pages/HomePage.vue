0710f2de3e65776a1ea602c1211af276a5b19e06
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-14 20:24:23 -0600

    feat: update questions

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -39,7 +39,7 @@
                 aria-expanded="false"
                 aria-controls="bs_accordion_faq_layout_2-collapseOne"
               >
-                Which plan should I choose?
+                Se Necesita Pagar?
               </button>
             </h2>
             <div
@@ -48,9 +48,8 @@
               aria-labelledby="bs_accordion_faq_layout_2-headingOne"
             >
               <div class="accordion-body">
-                Pick the 'Our' plan if it is only about brainstorming UI ideas. But if you want to
-                manage your entire product design workflow without restrictions, then choose the
-                ProductDesign plan.
+                No, todo en nuestro sitio es completamente gratis. En el futuro tal vez va a ver anuncios o una forma
+                para ayudarnos pero en el momento no.
               </div>
             </div>
           </div>
@@ -65,7 +64,7 @@
                 aria-expanded="false"
                 aria-controls="bs_accordion_faq_layout_2-collapseTwo"
               >
-                Should project members purchase a license?
+                Necesito saber matemáticas?
               </button>
             </h2>
             <div
@@ -74,137 +73,8 @@
               aria-labelledby="bs_accordion_faq_layout_2-headingTwo"
             >
               <div class="accordion-body">
-                Project members can review shared projects with the free basic plan. But need a license
-                to co-edit projects.
-              </div>
-            </div>
-          </div>
-
-          <div class="accordion-item my-4">
-            <h2 id="bs_accordion_faq_layout_2-headingThree" class="accordion-header">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#bs_accordion_faq_layout_2-collapseThree"
-                aria-expanded="false"
-                aria-controls="bs_accordion_faq_layout_2-collapseThree"
-              >
-                What forms of payment do you accept?
-              </button>
-            </h2>
-            <div
-              id="bs_accordion_faq_layout_2-collapseThree"
-              class="accordion-collapse collapse"
-              aria-labelledby="bs_accordion_faq_layout_2-headingThree"
-            >
-              <div class="accordion-body">
-                You can purchase with any major credit card. Enterprise licenses have
-                additional payment options such as bank transfer and Purchase Order using the same
-                purchase button in the above table.
-              </div>
-            </div>
-          </div>
-
-          <div class="accordion-item my-4">
-            <h2 id="bs_accordion_faq_layout_2-headingFour" class="accordion-header">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#bs_accordion_faq_layout_2-collapseFour"
-                aria-expanded="false"
-                aria-controls="bs_accordion_faq_layout_2-collapseFour"
-              >
-                Is there a trial available?
-              </button>
-            </h2>
-            <div
-              id="bs_accordion_faq_layout_2-collapseFour"
-              class="accordion-collapse collapse"
-              aria-labelledby="bs_accordion_faq_layout_2-headingFour"
-            >
-              <div class="accordion-body">
-                We offers a free Basic plan for all its apps. The basic plan has certain restrictions
-                like the number of projects and collaborators, but it still provides enough features to
-                learn and test without time limits.
-              </div>
-            </div>
-          </div>
-
-          <div class="accordion-item my-4">
-            <h2 id="bs_accordion_faq_layout_2-headingFive" class="accordion-header">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#bs_accordion_faq_layout_2-collapseFive"
-                aria-expanded="false"
-                aria-controls="bs_accordion_faq_layout_2-collapseFive"
-              >
-                What happens when my license expires?
-              </button>
-            </h2>
-            <div
-              id="bs_accordion_faq_layout_2-collapseFive"
-              class="accordion-collapse collapse"
-              aria-labelledby="bs_accordion_faq_layout_2-headingFive"
-            >
-              <div class="accordion-body">
-                When your subscription period ends, you will receive an email notification of the
-                license expiration. If not renewed, all your projects will be archived securely for free
-                and accessed when upgraded again.
-              </div>
-            </div>
-          </div>
-
-
-          <div class="accordion-item my-4">
-            <h2 id="bs_accordion_faq_layout_2-headingSix" class="accordion-header">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#bs_accordion_faq_layout_2-collapseSix"
-                aria-expanded="false"
-                aria-controls="bs_accordion_faq_layout_2-collapseSix"
-              >
-                Can I purchase additional user licenses later?
-              </button>
-            </h2>
-            <div
-              id="bs_accordion_faq_layout_2-collapseSix"
-              class="accordion-collapse collapse"
-              aria-labelledby="bs_accordion_faq_layout_2-headingSix"
-            >
-              <div class="accordion-body">
-                Yes. The license administrator can purchase additional user licenses anytime from their
-                dashboard.
-              </div>
-            </div>
-          </div>
-
-
-          <div class="accordion-item my-4">
-            <h2 id="bs_accordion_faq_layout_2-headingSeven" class="accordion-header">
-              <button
-                class="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#bs_accordion_faq_layout_2-collapseSeven"
-                aria-expanded="false"
-                aria-controls="bs_accordion_faq_layout_2-collapseSeven"
-              >
-                Is my account secure?
-              </button>
-            </h2>
-            <div
-              id="bs_accordion_faq_layout_2-collapseSeven"
-              class="accordion-collapse collapse"
-              aria-labelledby="bs_accordion_faq_layout_2-headingSeven"
-            >
-              <div class="accordion-body">
-                We follow various best practices to keep all data secure and backed up.
+                No, el desarrollo web casi no se necesita saber matemáticas. No mas necesitas las ganas de aprender Y tiempo.
+                También una computadora y conexión para nuestro sitio pero fuera de eso nada.
               </div>
             </div>
           </div>
