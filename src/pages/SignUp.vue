9436e1fa119cbffff06bb9c3c197ac3057cabaa4
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-15 23:25:30 -0600

    feat: made singIn into one button in NavBar feat: add signIn logic

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,7 +1,48 @@
 <template>
   <nav-bar />
+  <div v-show="login" class="bs_create_account_layout5 position-relative p-5">
+    <div class="container-fluid px-0">
+      <div class="col-lg-5 mx-auto">
+        <div
+          class="d-flex justify-center flex-column align-items-center bg-dark-subtle border-0 rounded p-5 bs_signup_content"
+        >
+          <p class="text-secondary h6 bs_fw-300 pb-3 text-white">
+            Iniciar sesión
+          </p>
+          <form class="py-3 pt-lg-3 w-100 md-mx-w-550" @submit.prevent="logIn">
+            <div class="mb-3">
+              <input
+                id="signup-5-Email"
+                v-model="email"
+                type="email"
+                class="form-control text-white"
+                placeholder="Correo Electrónico"
+                required
+              >
+            </div>
+            <div class="mb-3">
+              <input
+                id="signup-5-Password"
+                v-model="password"
+                type="password"
+                class="form-control text-white"
+                placeholder="Contraseña"
+                required
+              >
+            </div>
+            <button type="submit" class="btn btn-primary w-100">
+              Iniciar sesión
+            </button>
+            <p class="pt-3 small mb-0" style="color: lightgray;">
+              No tienes una Cuenta? <a class="text-decoration-none" @click.prevent="show">Crea una Cuenta</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
-  <div class="bs_create_account_layout5 position-relative p-5">
+  <div v-show="!login" class="bs_create_account_layout5 position-relative p-5">
     <div class="container-fluid px-0">
       <div class="col-lg-5 mx-auto">
         <div
@@ -46,7 +87,7 @@
               Crea Cuenta
             </button>
             <p class="pt-3 small mb-0" style="color: lightgray;">
-              Ya tienes una cuenta? <a href="#" class="text-decoration-none">Iniciar sesión</a>
+              Ya tienes una cuenta? <a class="text-decoration-none" @click.prevent="show">Iniciar sesión</a>
             </p>
           </form>
         </div>
@@ -60,6 +101,7 @@
 export default {
   data() {
     return {
+      login    : false,
       name     : "",
       email    : "",
       password : ""
@@ -72,6 +114,15 @@ export default {
       console.log("password ->", this.password);
 
       alert("Account has been created!");
+    },
+    logIn() {
+      console.log("email ->", this.email);
+      console.log("password ->", this.password);
+
+      alert("Logged In!");
+    },
+    show() {
+      this.login = !this.login;
     }
   }
 };
