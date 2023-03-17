7622b9f5927bde355d60e100bdea71f003ec2c5d
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-17 14:38:51 -0600

    feat: change title and description of sections

--- src/pages/lessons/ModulesPage.vue
+++ src/pages/lessons/ModulesPage.vue
@@ -7,27 +7,41 @@
       <li class="list-group-item d-flex justify-content-between align-items-start">
         <div class="ms-2 me-auto">
           <div class="fw-bold">
-            Subheading
+            Introducción a HTML
           </div>
-          Content for list item
+          En esta lección en video,
+          aprenderás todo lo básico sobre HTML, desde la estructura de una página hasta cómo agregar texto, imágenes y otros elementos.
+          Descubrirás por qué HTML es la base de todos los sitios web y cómo puedes utilizarlo para crear páginas web increíbles.
         </div>
         <span class="badge bg-primary rounded-pill">Completado</span>
       </li>
       <li class="list-group-item d-flex justify-content-between align-items-start">
         <div class="ms-2 me-auto">
           <div class="fw-bold">
-            Subheading
+            Problemas
           </div>
-          Content for list item
+          En esta sección, te presentaremos una serie de problemas que pondrán a prueba todo lo que aprendiste en la sección anterior.
+          Desde codificación hasta situaciones del mundo real, estos problemas te ayudarán a consolidar tu comprensión de los conceptos de desarrollo web
+          y a mejorar tus habilidades.
         </div>
         <span class="badge bg-primary rounded-pill">Completado</span>
       </li>
       <li class="list-group-item d-flex justify-content-between align-items-start">
         <div class="ms-2 me-auto">
           <div class="fw-bold">
-            Subheading
+            Introducción al Proyecto
           </div>
-          Content for list item
+          Introducción al mini proyecto, en este proyecto van a construir una pagina de
+          tu perfil, un portfolio.
+        </div>
+        <span class="badge bg-secondary rounded-pill">Aprende</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">
+            Lección Texto
+          </div>
+          En esta sección
         </div>
         <span class="badge bg-secondary rounded-pill">Aprende</span>
       </li>
