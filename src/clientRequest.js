3682ccb9d104cbea6ad7b226167db07125c9c043
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-17 11:23:27 -0600

    feat: add axios as deps feat: add example axios req

--- src/clientRequest.js
+++ src/clientRequest.js
@@ -1,2 +1,14 @@
 import axios from "axios";
 
+export const exampleCreateUser = async (uuid) => {
+    const param = `${uuid}`;
+
+    const data = await axios({
+        method : "get",
+        url    : `http://127.0.0.1:8080/users/${param}`
+    })
+        .then((response) => response.data)
+        .catch((err) => err);
+
+    return data.records;
+};
