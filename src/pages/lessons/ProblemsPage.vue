6d34728d0a2a197bd16eb9b5cdeed1f1d05004cb
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-21 17:07:43 -0600

    feat: add problemsPage example content

--- src/pages/lessons/ProblemsPage.vue
+++ src/pages/lessons/ProblemsPage.vue
@@ -1,9 +1,31 @@
 <template>
   <nav-bar />
-  <h1>Problems Page</h1>
-  <code>
-    <input>
-  </code>
+  <div class="container mt-3 mb-3 text-center">
+    <h1 class="fw-lighter fs-3">
+      {{ text }}
+    </h1>
+  </div>
+  <hr>
+
+  <div class="form-floating mt-3">
+    <textarea
+      id="floatingTextarea2"
+      class="form-control text-white"
+      placeholder="Leave a comment here"
+      style="height: 300px"
+    />
+    <label for="floatingTextarea2">Codigo</label>
+  </div>
 
   <nav-footer />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hola, en esta problema necesitan crear párrafo con una imagen"
+    };
+  }
+};
+</script>
