43bb8374ea9f5b565761159d5486a3d1a283b0c5
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-04-07 11:23:08 -0600

    chore: lint and delete unnecessary code

--- src/store/daysStore.js
+++ src/store/daysStore.js
@@ -13,15 +13,15 @@ export const useDaysStore = defineStore("days", {
         async getByContent(uuid = "") {
             const tempContent = [];
             const data = await getContentsRelationByDays(uuid);
-try {
+
             if (data) {
                 data.node.forEach((value) => {
                     const objTemp = {
-                        title: value.node.title,
-                        path: value.node.path,
-                        exp: value.node.exp,
-                        desc: value.node.desc,
-                        labels: value.node.labels
+                        title  : value.node.title,
+                        path   : value.node.path,
+                        exp    : value.node.exp,
+                        desc   : value.node.desc,
+                        labels : value.node.labels
                     };
 
                     if (objTemp.path) {
@@ -32,18 +32,14 @@ try {
                 this.result = [...tempContent];
             } else {
                 const fakeObj = {
-                    title: "Aún no hay contenido disponible",
-                    path: "Aún no hay contenido disponible",
-                    exp: "Aún no hay contenido disponible",
-                    desc: "Aún no hay contenido disponible",
-                    label: "Aún no hay contenido disponible"
+                    title : "Aún no hay contenido disponible",
+                    path  : "Aún no hay contenido disponible",
+                    exp   : "Aún no hay contenido disponible",
+                    desc  : "Aún no hay contenido disponible",
+                    label : "Aún no hay contenido disponible"
                 };
                 this.result[0] = fakeObj;
             }
-        } catch (error) {
-            console.log(error);
-            this.result = [];
-        }
         }
     },
     state: () => {
