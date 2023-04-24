1356bea11bd9b82c2b12e0b497e0fa211b6ea8b3
3fili3 <fpl08999@gmail.com>
2023-04-24 16:16:11 -0600

    Module Login Google

--- src/components/GLoginButton.vue
+++ src/components/GLoginButton.vue
@@ -1,6 +1,29 @@
 <template>
-    <GoogleLogin :callback="callback" />
+    <GoogleLogin :callback="callback" class="col-sm-12">
+        <button class="btn-google-logIn">
+            <div class="row">
+                <div class="col-2 img-google" ></div>
+                <div class="col-10 text-center" >
+                    Iniciar / Crear cuenta con Google
+                </div>
+            </div>
+        </button>
+    </GoogleLogin>
 </template>
+
+<style scoped>
+.btn-google-logIn {
+    width: 100%;
+    height: 45px;
+    border: none !important;
+}
+.img-google {
+    background-image: url('../pages/assets/google.png');
+    background-repeat: no-repeat;
+    background-size: 20px;
+    background-position: 50%;
+}
+</style>
 
 <script>
 import { decodeCredential } from "vue3-google-login";
@@ -11,15 +34,20 @@ export default {
     methods: {
         ...mapActions(useAuthStore, ["setEmail", "setName", "setPassword"]),
         callback(response) {
-            const userData = decodeCredential(response.credential);
-            this.$emit("credential", response.credential);
-            this.setEmail(userData.email);
-            
-            let userName = userData.email.split("@")[0];
-
-            this.setName(userName);
-            this.setPassword(userData.jti);
-            this.$emit("logged-in", true);
+            try {
+                const userData = decodeCredential(response.credential);
+                this.$emit("credential", response.credential);
+                this.setEmail(userData.email);
+                
+                let userName = userData.email.split("@")[0];
+    
+                this.setName(userName);
+                this.setPassword(userData.jti);
+                this.$emit("logged-in", true);
+                
+            } catch (error) {
+                console.log(error)
+            }
         }
     }
 };
--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,106 +1,112 @@
 <template>
   <nav-bar />
-  <div v-show="login" class="bs_create_account_layout5 position-relative p-5">
-    <div class="container-fluid px-0">
-      <div class="col-lg-5 mx-auto">
-        <div
-          class="d-flex justify-center flex-column align-items-center bg-dark-subtle border-0 rounded p-5 bs_signup_content"
-        >
-          <p class="text-secondary h6 bs_fw-300 pb-3 text-white">
-            Iniciar sesión
-          </p>
-          <form class="py-3 pt-lg-3 w-100 md-mx-w-550" @submit.prevent="logIn">
-            <div class="mb-3">
-              <input
-                id="signup-5-Email"
-                v-model="email"
-                type="email"
-                class="form-control text-white"
-                placeholder="Correo Electrónico"
-                required
-              >
+
+  <div class="d-flex justify-content-center mt-5">
+    <div class="col-sm-4 col-12">
+      <div class="position-btn-google bg-primary">
+            <g-login class="col-12" @logged-in="checkGLogin" @credential="fillCredential" />
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+
+          <div v-show="login" class="bs_create_account_layout5 position-relative">
+            <div class="container-fluid px-0">
+              <div class="col-lg-12 mx-auto">
+                <div
+                  class="d-flex justify-center flex-column align-items-center bg-dark-subtle border-0 rounded p-5 bs_signup_content">
+                  <p class="text-secondary h6 bs_fw-300 pb-3 text-white">
+                    Iniciar sesión
+                  </p>
+                  <form class="py-3 pt-lg-3 w-100 md-mx-w-550" @submit.prevent="logIn">
+                    <div class="mb-3">
+                      <input id="signup-5-Email" v-model="email" type="email" class="form-control text-white"
+                        placeholder="Correo Electrónico" required />
+                    </div>
+                    <div class="mb-3">
+                      <input id="signup-5-Password" v-model="password" type="password" class="form-control text-white"
+                        placeholder="Contraseña" required />
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100 text-white">
+                      Iniciar sesión
+                    </button>
+
+                    <p class="pt-3 small mb-0" style="color: lightgray">
+                      No tienes una Cuenta?
+                      <a class="text-decoration-none" @click.prevent="show">Crea una Cuenta</a>
+                    </p>
+                  </form>
+                </div>
+              </div>
             </div>
-            <div class="mb-3">
-              <input
-                id="signup-5-Password"
-                v-model="password"
-                type="password"
-                class="form-control text-white"
-                placeholder="Contraseña"
-                required
-              >
+          </div>
+
+          <div v-show="!login" class="bs_create_account_layout5 position-relative">
+            <div class="container-fluid px-0">
+              <div class="col-lg-12 mx-auto">
+                <div
+                  class="d-flex justify-center flex-column align-items-center bg-dark-subtle border-0 rounded p-5 bs_signup_content">
+                  <p class="text-secondary h6 bs_fw-300 pb-3 text-white">
+                    Crea una Cuenta
+                  </p>
+                  <form class="py-3 pt-lg-3 w-100 md-mx-w-550" @submit.prevent="createAccount(false)">
+                    <div class="mb-3">
+                      <input id="signup-5-name" v-model="userName" type="text" class="form-control text-white"
+                        placeholder="Nombre" aria-describedby="infotext" required />
+                    </div>
+                    <div class="mb-3">
+                      <input id="signup-5-Email" v-model="email" type="email" class="form-control text-white"
+                        placeholder="Correo Electrónico" required />
+                    </div>
+                    <div class="mb-3">
+                      <input id="signup-5-Password" v-model="password" type="password" class="form-control text-white"
+                        placeholder="Contraseña" required />
+                    </div>
+                    <div v-if="gLogin && password === ''" class="alert alert-warning" role="alert">
+                      Introduzca su contraseña
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100 text-white">
+                      Crea Cuenta
+                    </button>
+
+                    <p class="pt-3 small mb-0" style="color: lightgray">
+                      Ya tienes una cuenta?
+                      <a class="text-decoration-none" @click.prevent="show">Iniciar sesión</a>
+                    </p>
+                  </form>
+                </div>
+              </div>
             </div>
-            <button type="submit" class="btn btn-primary w-100 text-white">
-              Iniciar sesión
-            </button>
-            <g-login @logged-in="checkGLogin" @credential="fillCredential" />
-            <p class="pt-3 small mb-0" style="color: lightgray;">
-              No tienes una Cuenta? <a class="text-decoration-none" @click.prevent="show">Crea una Cuenta</a>
-            </p>
-          </form>
+          </div>
+
         </div>
+      
       </div>
     </div>
+
   </div>
 
-  <div v-show="!login" class="bs_create_account_layout5 position-relative p-5">
-    <div class="container-fluid px-0">
-      <div class="col-lg-5 mx-auto">
-        <div
-          class="d-flex justify-center flex-column align-items-center bg-dark-subtle border-0 rounded p-5 bs_signup_content"
-        >
-          <p class="text-secondary h6 bs_fw-300 pb-3 text-white">
-            Crea una Cuenta
-          </p>
-          <form class="py-3 pt-lg-3 w-100 md-mx-w-550" @submit.prevent="createAccount(false)">
-            <div class="mb-3">
-              <input
-                id="signup-5-name"
-                v-model="userName"
-                type="text"
-                class="form-control text-white"
-                placeholder="Nombre"
-                aria-describedby="infotext"
-                required
-              >
-            </div>
-            <div class="mb-3">
-              <input
-                id="signup-5-Email"
-                v-model="email"
-                type="email"
-                class="form-control text-white"
-                placeholder="Correo Electrónico"
-                required
-              >
-            </div>
-            <div class="mb-3">
-              <input
-                id="signup-5-Password"
-                v-model="password"
-                type="password"
-                class="form-control text-white"
-                placeholder="Contraseña"
-                required
-              >
-            </div>
-            <div v-if="gLogin && password === ''" class="alert alert-warning" role="alert">
-              Introduzca su contraseña
-            </div>
-            <button type="submit" class="btn btn-primary w-100 text-white">
-              Crea Cuenta
-            </button>
-            <g-login @logged-in="checkGLogin" @credential="fillCredential" />
-            <p class="pt-3 small mb-0" style="color: lightgray;">
-              Ya tienes una cuenta? <a class="text-decoration-none" @click.prevent="show">Iniciar sesión</a>
-            </p>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
+
   <nav-footer />
 </template>
+
+<style>
+.position-btn-google {
+  width: 350px;
+  position: absolute;
+  left: 82.5%;
+  top: 12.5%;
+  transform: translate(-82.5%, -12.5%);
+  z-index: 50;
+}
+
+@media only screen and (max-width: 600px) {
+  .position-btn-google {
+    width: 100%;
+    left: 0% !important;
+    transform: translate(0%, -12.5%);
+  }
+}
+</style>
 
 <script>
 import { mapActions, mapState } from "pinia";
@@ -112,31 +118,36 @@ import GLoginButton from "@/components/GLoginButton.vue";
 
 export default {
   components: {
-    "g-login": GLoginButton
+    "g-login": GLoginButton,
   },
   data() {
     return {
-      login      : false,
-      userName   : "",
-      email      : "",
-      password   : "",
-        gLogin     : false,
-        credential : ""
+      login: false,
+      userName: "",
+      email: "",
+      password: "",
+      gLogin: false,
+      credential: "",
     };
   },
   computed: {
-    ...mapState(useAuthStore, ["storeName", "storeEmail", "storePassword"])
+    ...mapState(useAuthStore, ["storeName", "storeEmail", "storePassword"]),
   },
   methods: {
-    ...mapActions(useUserStore, ["createUser", "logInUser", "createGoogleUser", "verifyUser"]),
+    ...mapActions(useUserStore, [
+      "createUser",
+      "logInUser",
+      "createGoogleUser",
+      "verifyUser",
+    ]),
     ...mapActions(useAuthStore, ["addAuthToken"]),
-      fillCredential(value) {
-        this.credential = value;
-      },
+    fillCredential(value) {
+      this.credential = value;
+    },
     async checkGLogin(value) {
-        let verified = await this.verifyUser(this.credential);
+      let verified = await this.verifyUser(this.credential);
       if (value) {
-          this.gLogin = true;
+        this.gLogin = true;
         if (this.storeEmail !== "") {
           this.email = this.storeEmail;
         }
@@ -144,38 +155,36 @@ export default {
           this.userName = this.storeName;
         }
 
-        if (verified)
-            await this.createAccount(true);
-        }
+        if (verified) await this.createAccount(true);
+      }
     },
     async createAccount(google) {
-        if (!google) {
-            const userObj = {
-                name     : this.userName,
-                email    : this.email,
-                password : this.password
-            };
+      if (!google) {
+        const userObj = {
+          name: this.userName,
+          email: this.email,
+          password: this.password,
+        };
 
-            const token = await this.createUser(userObj);
+        const token = await this.createUser(userObj);
 
-            this.addAuthToken(token);
-        } else {
-            const userObj = {
-                name  : this.userName,
-                email : this.email,
-                token : this.credential
-            };
+        this.addAuthToken(token);
+      } else {
+        const userObj = {
+          name: this.userName,
+          email: this.email,
+          token: this.credential,
+        };
 
-            const token = await this.createGoogleUser(userObj);
+        const token = await this.createGoogleUser(userObj);
 
-            this.addAuthToken(token);
-        }
-
+        this.addAuthToken(token);
+      }
     },
     async logIn() {
       const userObj = {
-        email    : this.email,
-        password : this.password
+        email: this.email,
+        password: this.password,
       };
 
       const token = await this.logInUser(userObj);
@@ -183,7 +192,7 @@ export default {
     },
     show() {
       this.login = !this.login;
-    }
-  }
+    },
+  },
 };
 </script>
