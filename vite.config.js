7b39a769351eba860235370427ce7654aaff22ab
Ember96 <gnomeaddict96@gmail.com>
2023-05-14 14:04:53 -0700

    added https support and new GCP credentials in preparation for cypress validation tests

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins : [vue()],
+  plugins : [vue(), basicSsl()],
   resolve : {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
