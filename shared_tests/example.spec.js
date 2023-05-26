c6019885495bd793afd1b264a18f562de5601554
Ember96 <gnomeaddict96@gmail.com>
2023-05-25 23:31:27 -0700

    HomeComponent testing running

--- shared_tests/example.spec.js
+++ shared_tests/example.spec.js
@@ -1,14 +1,23 @@
+import { createTestingPinia } from "@pinia/testing"
+
 import{
 describe, it, expect
 } from "vitest";
+import { fn } from "@vitest/spy";
 import HomePage from "./src/pages/HomePage.vue";
 import { mount } from "@vue/test-utils";
 import {useAuthStore} from "@/store/user/authStore.js";
 
+const wrapper = mount(HomePage, {
+    global: {
+        plugins: [createTestingPinia({createSpy: fn})]
+    }
+});
+
 describe("HomePage", () => {
     it("should render home page", () => {
         const authStore = useAuthStore();
-        const wrapper = mount(HomePage, { global: { plugins: [authStore] } });
-        expect(HomePage.props.title).toContain("Code");
+        const wrapper = mount(HomePage, { });
+        expect(wrapper.text()).toContain("Code");
     });
 });
