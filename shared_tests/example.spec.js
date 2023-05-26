affba5738f6fdb50d68c4073e57f374e777d2367
Ember96 <gnomeaddict96@gmail.com>
2023-05-25 23:40:35 -0700

    removing unused Store and wrapper given that vitest provides its own store

--- shared_tests/example.spec.js
+++ shared_tests/example.spec.js
@@ -6,18 +6,14 @@ describe, it, expect
 import { fn } from "@vitest/spy";
 import HomePage from "./src/pages/HomePage.vue";
 import { mount } from "@vue/test-utils";
-import {useAuthStore} from "@/store/user/authStore.js";
 
 const wrapper = mount(HomePage, {
     global: {
         plugins: [createTestingPinia({createSpy: fn})]
     }
 });
-
 describe("HomePage", () => {
     it("should render home page", () => {
-        const authStore = useAuthStore();
-        const wrapper = mount(HomePage, { });
         expect(wrapper.text()).toContain("Code");
     });
 });
