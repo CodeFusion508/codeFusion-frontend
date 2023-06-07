162f2fbbb2ec478357336d1aafde4b889967dca5
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-06-06 18:35:41 -0600

    fix: css and html issue in SignUp fix: change google API to env

--- src/config/auth.js
+++ src/config/auth.js
@@ -3,6 +3,6 @@ import vue3GoogleLogin from "vue3-google-login";
 import { app } from "./config.js";
 
 app.use(vue3GoogleLogin, {
-  clientId : "1079746536463-hu5gv65n5jin72ee5s4gt7de5n7qhs4e.apps.googleusercontent.com",
+  clientId : import.meta.env.VITE_GOOGLE_API,
   scope    : "profile email openid"
 });
