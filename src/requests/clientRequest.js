92fa9a43b6f9bf25fd97886c775e5107a50a0d94
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-06-16 12:48:10 -0600

    chore: correct path to backend

--- src/requests/clientRequest.js
+++ src/requests/clientRequest.js
@@ -25,6 +25,6 @@ export const recoveryAccount = async (email) => (await request("POST", "/recover
 // Google User Requests
 const requestGoogle = HTTP("Google", "google");
 
-export const createGoogleUserReq = async ({ name, email, token }) => (await requestGoogle("POST", ``, { userName: name, email, idToken: token }));
+export const createGoogleUserReq = async ({ name, email, token }) => (await requestGoogle("POST", `/users/signUp`, { userName: name, email, idToken: token }));
 
-export const verifyGUserReq = async (dataRequest) => (await requestGoogle("POST", "/ver", { idToken: dataRequest }));
+export const verifyGUserReq = async (dataRequest) => (await requestGoogle("POST", "/users/logIn", { idToken: dataRequest }));
