8dc3b811fe236e8ad0234b7e2a845fac19b49150
Ember96 <gnomeaddict96@gmail.com>
2023-07-11 16:01:13 -0700

    UserSettings spec check if all input are loaded and get the correct values from store and update store once input value changes

--- src/pages/tests/UserSettings.spec.js
+++ src/pages/tests/UserSettings.spec.js
@@ -5,7 +5,7 @@ import{
 } from "vitest";
 import { fn } from "@vitest/spy";
 import UserSettings from "@/pages/UserSettings.vue";
-import { mount } from "@vue/test-utils";
+import {mount} from "@vue/test-utils";
 import {useUserStore} from "@/store/user/userStore.js";
 
 let userObj = {
@@ -16,17 +16,39 @@ let userObj = {
     tkn      : "qwerty123456",
     password : "asdasdadadasdasda"
 };
+
 const wrapper = mount(UserSettings, {
     global: {
-        plugins: [createTestingPinia({initialState: { UserSettings: { userObj: userObj}} ,createSpy: fn})]
+        plugins: [createTestingPinia({createSpy: fn})]
     }
 });
 
 const store = useUserStore();
 store.userObj = userObj;
 describe("UserSettings", () => {
-    it("should show user info", () => {
-        expect(wrapper.text()).toContain("manolo");
-        expect(wrapper.text()).toContain("another@email.com");
+    let inputs = wrapper.findAll("input");
+    it("should show inputs", function () {
+        expect(inputs.length).toBe(4);
+    });
+    it("should show user' name", () => {
+        expect(inputs[1].element.value).toBe("manolo");
+    });
+    it("should show user' email", () => {
+        expect(inputs[2].element.value).toBe(userObj.email);
+    });
+    it("should show user' password", () => {
+        expect(inputs[3].element.value).toBe(userObj.password);
+    });
+    it("should update username",  () => {
+        inputs[1].setValue("pepe");
+        expect(wrapper.vm.userObj.name).toBe("pepe");
+    });
+    it("should update email",  () => {
+        inputs[2].setValue("anyother@ooutlook.took");
+        expect(wrapper.vm.userObj.email).toBe("anyother@ooutlook.took");
+    });
+    it("should update password",  () => {
+        inputs[3].setValue("123456789");
+        expect(wrapper.vm.userObj.password).toBe("123456789");
     });
 });
