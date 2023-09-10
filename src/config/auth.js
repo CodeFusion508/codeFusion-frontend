eab00a9c41138f8a8630be416520614473037b5a
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-09-10 13:02:07 -0700

    chore: remove unnecessary example test style: merge all configs in main.js

--- src/config/auth.js
+++
@@ -1,8 +0,0 @@
-import vue3GoogleLogin from "vue3-google-login";
-
-import { app } from "./config.js";
-
-app.use(vue3GoogleLogin, {
-  clientId : import.meta.env.VITE_GOOGLE_CLIENT_ID || "no ID for now",
-  scope    : "profile email openid"
-});
