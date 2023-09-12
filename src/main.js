3d4ad50b02293feba9d35cc5da4a51cee61b6dd1
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-09-11 19:38:25 -0700

    chore: remove unnecessary package fix: issues with import on main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // App construction and initialization
 import { createApp } from "vue";
 
-import App from "../App.vue";
+import App from "./App.vue";
 
 export const app = createApp(App);
 
 // App plugins and uses
 import { createPinia } from "pinia";
-import router from "../router/router.js";
+import router from "./router/router.js";
 import { install } from "@guolao/vue-monaco-editor";
 import vue3GoogleLogin from "vue3-google-login";
 
@@ -18,12 +18,12 @@ app.use(vue3GoogleLogin, {
     clientId : import.meta.env.VITE_GOOGLE_CLIENT_ID || "no ID for now",
     scope    : "profile email openid"
 });
-
+console.log(app);
 
 // Global components
-import NavBar from "../components/NavBar.vue";
-import NavFooter from "../components/NavFooter.vue";
-import ToastError from "../components/ToastError.vue";
+import NavBar from "./components/NavBar.vue";
+import NavFooter from "./components/NavFooter.vue";
+import ToastError from "./components/ToastError.vue";
 
 app.component("nav-bar", NavBar);
 app.component("nav-footer", NavFooter);
